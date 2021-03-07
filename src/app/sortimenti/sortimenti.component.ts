764b7672839aa022576d9c5a762aafdbed67c253
Aleksandar Kovac <alexkovach@hotmail.com>
2021-03-07 16:20:06 +0100

    test racunanja cjene na sortimentComponenti

--- src/app/sortimenti/sortimenti.component.ts
+++ src/app/sortimenti/sortimenti.component.ts
@@ -7,6 +7,10 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./sortimenti.component.css']
 })
 export class SortimentiComponent implements OnInit {
+  sumKlasa: number;
+  sumNormaTest: number;
+  sumTroskova: number;
+  sumCjenaProjekta: number;
   sortimentiFormGroup = new FormGroup({
     fTrupci: new FormControl(''),
     lTrupci: new FormControl(''),
@@ -22,9 +26,18 @@ export class SortimentiComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onSubmit() {
-    console.log(this.sortimentiFormGroup.value);
+  onSubmit(): void {
+    // console.log(this.sortimentiFormGroup.value);
+    const novaFtrupci = this.sortimentiFormGroup.get('fTrupci').value;
+    const novaLtrupci = this.sortimentiFormGroup.get('lTrupci').value;
     const novaPrvaKlasa = this.sortimentiFormGroup.get('prvaKlasa').value;
-    console.log(novaPrvaKlasa);
+    const novaDrugaKlasa = this.sortimentiFormGroup.get('drugaKlasa').value;
+    const novaTrecaKlasa = this.sortimentiFormGroup.get('trecaKlasa').value;
+    this.sumKlasa = novaFtrupci + novaLtrupci + novaPrvaKlasa + novaDrugaKlasa + novaTrecaKlasa;
+
+    this.sumNormaTest = 5.10;
+    this.sumTroskova = 30000;
+    this.sumCjenaProjekta = this.sumTroskova / this.sumKlasa;
+    // console.log(this.sumKlasa);
   }
 }
